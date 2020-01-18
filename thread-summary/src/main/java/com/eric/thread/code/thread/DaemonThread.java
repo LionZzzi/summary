820067d0a0467e1df46fb5ae76d2eacaf48d96e1
LionZzzi <37397058+LionZzzi@users.noreply.github.com>
2020-01-18 23:04:33 +0800

    守护线程

---
+++ thread-summary/src/main/java/com/eric/thread/code/thread/DaemonThread.java
@@ -0,0 +1,30 @@
+package com.eric.thread.code.thread;
+
+/**
+ * @author Eric
+ * @date 2020/1/17 22:51
+ */
+public class DaemonThread {
+
+    private static class UseDaemon extends Thread {
+        public UseDaemon(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+                System.out.println("thread name :" + Thread.currentThread().getName());
+            }
+            System.out.println("end :" + Thread.currentThread().getName());
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread deamon = new UseDaemon("daemon");
+        deamon.setDaemon(true);
+        deamon.start();
+        Thread.sleep(5);
+        //deamon.interrupt();
+    }
+}
