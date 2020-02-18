2723c55140802cc1fdff707a68ff6e9c07e91c3a
LionZzzi <37397058+LionZzzi@users.noreply.github.com>
2020-02-18 23:35:46 +0800

    ReentrantLockTest.java

--- thread-summary/src/main/java/com/eric/thread/code/util/ReentrantLockTest.java
+++ thread-summary/src/main/java/com/eric/thread/code/util/ReentrantLockTest.java
@@ -1,5 +1,6 @@
 package com.eric.thread.code.util;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,9 +9,47 @@ import java.util.concurrent.locks.ReentrantLock;
  * @date 2020/2/17 0:04
  */
 public class ReentrantLockTest {
-    private static final Lock lock = new ReentrantLock();
+    private static final ReentrantLock lock = new ReentrantLock();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        // IntStream.range(0, 2).forEach(thread -> new Thread(ReentrantLockTest::lock, thread + " name").start());
+
+        new Thread(() -> {
+            unInterruptedLock();
+        }).start();
+        TimeUnit.SECONDS.sleep(1);
+        Thread thread = new Thread(() -> {
+            unInterruptedLock();
+        });
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+
+        lock.getHoldCount();
+    }
+
+    public static void lock() {
+        lock.lock();
+        try {
+            System.out.println(System.currentTimeMillis() + "工作5秒" + Thread.currentThread().getName());
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(System.currentTimeMillis() + "结束工作" + Thread.currentThread().getName());
             lock.unlock();
+        }
+    }
+
+    public static void unInterruptedLock() {
+        try {
+            lock.lockInterruptibly();
+            while (true) {
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 }
